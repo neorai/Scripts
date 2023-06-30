f87c3f72b77507a9d0bb3a49503460a167e02920
Robert <32270269+neorai@users.noreply.github.com>
2023-06-30 23:12:48 +0200

    arreglar modificaciones de prueba

--- python/buscar_nombres_de_archivo.py
+++ python/buscar_nombres_de_archivo.py
@@ -3,20 +3,15 @@ import os
 def buscar_por_cadena(carpeta, cadena):
     resultados = []
     for ruta_actual, carpetas, archivos in os.walk(carpeta):
-        """for archivo in archivos:
+        for archivo in archivos:
             if archivo.lower().find(cadena.lower()) != -1:
                 ruta_completa = os.path.join(ruta_actual, archivo)
-                resultados.append(ruta_completa)"""
+                resultados.append(ruta_completa)
         for carpeta in carpetas:
             if carpeta.lower().find(cadena.lower()) != -1:
                 ruta_completa = os.path.join(ruta_actual, carpeta)
                 resultados.append(ruta_completa)
-                for ruta_actual, carpetas, archivos in os.walk(ruta_completa):
-                    for archivo in archivos:
-                        if archivo.lower().find(cadena.lower()) != -1:
-                            ruta_completa_archivo = os.path.join(ruta_actual, archivo)
-                            resultados.append(ruta_completa_archivo)
-    return resultados
+
 
 def guardar_resultados(resultados, nombre_archivo):
     with open(nombre_archivo, 'w', encoding='utf-8') as archivo:
